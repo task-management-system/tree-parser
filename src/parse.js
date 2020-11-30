7e6f7ee374c51a4c3a2370a3fa8ac46de92d87d4
Alexander Munko <seasle98@gmail.com>
2020-11-30 13:54:40 +0600

    Конвертация табуляций в пробелы

--- src/parse.js
+++ src/parse.js
@@ -1,60 +1,60 @@
 const parse = text => {
-	const tree = [];
+    const tree = [];
 
-	const cache = new Map();
-	const lines = text.split(/\r\n|\r|\n/g);
-	const path = [];
-	let previousLevel = null;
+    const cache = new Map();
+    const lines = text.split(/\r\n|\r|\n/g);
+    const path = [];
+    let previousLevel = null;
 
-	for (const line of lines) {
-		const name = line.trim();
-		if (name === '') {
-			continue;
-		}
+    for (const line of lines) {
+        const name = line.trim();
+        if (name === '') {
+            continue;
+        }
 
-		const level = (line.match(/^\s+/g) || [''])[0].length;
-		const lastLevel = path[path.length - 1];
-		if (lastLevel !== level) {
-			if (lastLevel > level) {
-				const difference = lastLevel - level;
-				for (let index = 0; index < difference; index++) {
-					const level = path.pop();
-					const key = [...cache.keys()].find(entry => entry.endsWith(level));
+        const level = (line.match(/^\s+/g) || [''])[0].length;
+        const lastLevel = path[path.length - 1];
+        if (lastLevel !== level) {
+            if (lastLevel > level) {
+                const difference = lastLevel - level;
+                for (let index = 0; index < difference; index++) {
+                    const level = path.pop();
+                    const key = [...cache.keys()].find(entry => entry.endsWith(level));
 
-					if (key !== undefined) {
-						cache.delete(key);
-					}
-				}
-			} else {
-				path.push(level);
-			}
-		}
+                    if (key !== undefined) {
+                        cache.delete(key);
+                    }
+                }
+            } else {
+                path.push(level);
+            }
+        }
 
-		if (path.length === 0) {
-			path.push(0);
-		}
+        if (path.length === 0) {
+            path.push(0);
+        }
 
-		const key = path.join('_');
-		const parentKey = path.slice(0, path.length - 1).join('_');
-		const branch = {
-			name,
-			children: []
-		};
+        const key = path.join('_');
+        const parentKey = path.slice(0, path.length - 1).join('_');
+        const branch = {
+            name,
+            children: []
+        };
 
-		if (!cache.has(key) || previousLevel >= level) {
-			cache.set(key, branch);
-		}
+        if (!cache.has(key) || previousLevel >= level) {
+            cache.set(key, branch);
+        }
 
-		if (cache.has(parentKey)) {
-			cache.get(parentKey).children.push(branch);
-		} else {
-			tree.push(branch);
-		}
+        if (cache.has(parentKey)) {
+            cache.get(parentKey).children.push(branch);
+        } else {
+            tree.push(branch);
+        }
 
-		previousLevel = level;
-	}
+        previousLevel = level;
+    }
 
-	return tree;
+    return tree;
 };
 
 export default parse;
